58d6d1352b49106f7040026f731dd037e23e40ad
andrey.veremyev <andrey.veremyev@megafon.ru>
2020-06-17 19:13:47 +0300

    Base and navigation setup

---
+++ app/src/main/java/com/example/android/muicapp/fragment/SamplesFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.muicapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.android.muicapp.R
+import kotlinx.android.synthetic.main.fragment_samples.*
+
+class SamplesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =inflater.inflate(R.layout.fragment_samples, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
