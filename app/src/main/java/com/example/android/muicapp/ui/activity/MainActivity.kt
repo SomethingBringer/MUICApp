5628d024be70ffa889f9b41c162aba144a045c39
SomethingBringer <verannick@gmail.com>
2020-06-26 18:28:20 +0300

    Main screen content

--- app/src/main/java/com/example/android/muicapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/android/muicapp/ui/activity/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
     lateinit var appBarConfiguration: AppBarConfiguration
-    val appComponent = DaggerAppComponent.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
