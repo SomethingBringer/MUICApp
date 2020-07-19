0576facaf9316f777cfb822507bee247374e035b
SomethingBringer <verannick@gmail.com>
2020-07-19 14:25:57 +0300

    refactor

--- app/src/main/java/com/example/android/muicapp/ui/fragment/notifications/basic/BasicNotificationFragment.kt
+++ app/src/main/java/com/example/android/muicapp/ui/fragment/notifications/basic/BasicNotificationFragment.kt
@@ -1,4 +1,4 @@
-package com.example.android.muicapp.ui.fragment.notifications
+package com.example.android.muicapp.ui.fragment.notifications.basic
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
